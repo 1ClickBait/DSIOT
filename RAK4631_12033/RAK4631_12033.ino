50aa46501f2e4c363c298f8548fe6991719af733
1ClickBait <cptxappa2583@gmail.com>
2025-03-29 14:25:48 +0000

    Add files via upload

---
+++ RAK4631_12033/RAK4631_12033.ino
@@ -0,0 +1,295 @@
+/**
+ * @file RAK4631-DeepSleep-LoRaWan.ino
+ * @author Bernd Giesecke (bernd.giesecke@rakwireless.com)
+ * @brief LoRaWan deep sleep example
+ * Device goes into sleep after successful OTAA/ABP network join.
+ * Wake up every SLEEP_TIME seconds. Set time in main.h 
+ * @version 0.1
+ * @date 2020-09-05
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ * @note RAK4631 GPIO mapping to nRF52840 GPIO ports
+   RAK4631    <->  nRF52840
+   WB_IO1     <->  P0.17 (GPIO 17)
+   WB_IO2     <->  P1.02 (GPIO 34)
+   WB_IO3     <->  P0.21 (GPIO 21)
+   WB_IO4     <->  P0.04 (GPIO 4)
+   WB_IO5     <->  P0.09 (GPIO 9)
+   WB_IO6     <->  P0.10 (GPIO 10)
+   WB_SW1     <->  P0.01 (GPIO 1)
+   WB_A0      <->  P0.04/AIN2 (AnalogIn A2)
+   WB_A1      <->  P0.31/AIN7 (AnalogIn A7)
+ */
+#include "main.h"
+#include "RAK12033-IIM42652.h"
+IIM42652_axis_t  accel_data;
+  IIM42652_axis_t  gyro_data;
+  float temp;
+
+  float acc_x ,acc_y ,acc_z;
+  float gyro_x,gyro_y,gyro_z;
+
+IIM42652 IMU;
+/** Semaphore used by events to wake up loop task */
+SemaphoreHandle_t taskEvent = NULL;
+
+/** Timer to wakeup task frequently and send message */
+SoftwareTimer taskWakeupTimer;
+
+/** Buffer for received LoRaWan data */
+uint8_t rcvdLoRaData[256];
+/** Length of received data */
+uint8_t rcvdDataLen = 0;
+
+/**
+ * @brief Flag for the event type
+ * -1 => no event
+ * 0 => LoRaWan data received
+ * 1 => Timer wakeup
+ * 2 => tbd
+ * ...
+ */
+uint8_t eventType = -1;
+
+/**
+ * @brief Timer event that wakes up the loop task frequently
+ * 
+ * @param unused 
+ */
+void periodicWakeup(TimerHandle_t unused)
+{
+	// Switch on blue LED to show we are awake
+	digitalWrite(LED_BUILTIN, HIGH);
+	eventType = 1;
+	// Give the semaphore, so the loop task will wake up
+	xSemaphoreGiveFromISR(taskEvent, pdFALSE);
+}
+
+/**
+ * @brief Arduino setup function. Called once after power-up or reset
+ * 
+ */
+void setup(void)
+{
+	// Create the LoRaWan event semaphore
+	taskEvent = xSemaphoreCreateBinary();
+	// Initialize semaphore
+	xSemaphoreGive(taskEvent);
+
+	// Initialize the built in LED
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+
+	// Initialize the connection status LED
+	pinMode(LED_CONN, OUTPUT);
+	digitalWrite(LED_CONN, HIGH);
+
+#ifndef MAX_SAVE
+	// Initialize Serial for debug output
+	Serial.begin(115200);
+
+	time_t timeout = millis();
+	// On nRF52840 the USB serial is not available immediately
+	while (!Serial)
+	{
+		if ((millis() - timeout) < 5000)
+		{
+			delay(100);
+			digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+		}
+		else
+		{
+			break;
+		}
+	}
+#endif
+   Wire.begin();
+  if (IMU.begin() == false) 
+  {
+    while (1) 
+    {
+      Serial.println("IIM-42652 is not connected.");
+      delay(5000);
+    }
+  }
+	digitalWrite(LED_BUILTIN, LOW);
+
+#ifndef MAX_SAVE
+	Serial.println("=====================================");
+	Serial.println("RAK4631 LoRaWan Deep Sleep Test");
+	Serial.println("=====================================");
+#endif
+
+	// Initialize LoRaWan and start join request
+	int8_t loraInitResult = initLoRaWan();
+
+#ifndef MAX_SAVE
+	if (loraInitResult != 0)
+	{
+		switch (loraInitResult)
+		{
+		case -1:
+			Serial.println("SX126x init failed");
+			break;
+		case -2:
+			Serial.println("LoRaWan init failed");
+			break;
+		case -3:
+			Serial.println("Subband init error");
+			break;
+		case -4:
+			Serial.println("LoRa Task init error");
+			break;
+		default:
+			Serial.println("LoRa init unknown error");
+			break;
+		}
+
+		// Without working LoRa we just stop here
+		while (1)
+		{
+			Serial.println("Nothing I can do, just loving you");
+			delay(5000);
+		}
+	}
+	Serial.println("LoRaWan init success");
+#endif
+
+	// Take the semaphore so the loop will go to sleep until an event happens
+	xSemaphoreTake(taskEvent, 10);
+}
+
+/**
+ * @brief Arduino loop task. Called in a loop from the FreeRTOS task handler
+ * 
+ */
+void loop(void)
+{
+	// Switch off blue LED to show we go to sleep
+	digitalWrite(LED_BUILTIN, LOW);
+
+	// Sleep until we are woken up by an event
+	if (xSemaphoreTake(taskEvent, portMAX_DELAY) == pdTRUE)
+	{
+		// Switch on blue LED to show we are awake
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(500); // Only so we can see the blue LED
+
+		// Check the wake up reason
+		switch (eventType)
+		{
+		case 0: // Wakeup reason is package downlink arrived
+#ifndef MAX_SAVE
+			Serial.println("Received package over LoRaWan");
+#endif
+			if (rcvdLoRaData[0] > 0x1F)
+			{
+#ifndef MAX_SAVE
+				Serial.printf("%s\n", (char *)rcvdLoRaData);
+#endif
+			}
+			else
+			{
+#ifndef MAX_SAVE
+				for (int idx = 0; idx < rcvdDataLen; idx++)
+				{
+					Serial.printf("%X ", rcvdLoRaData[idx]);
+				}
+				Serial.println("");
+#endif
+			}
+
+			break;
+		case 1: // Wakeup reason is timer
+#ifndef MAX_SAVE
+			Serial.println("Timer wakeup");
+#endif
+			/// \todo read sensor or whatever you need to do frequently
+      IMU.ex_idle();
+  IMU.accelerometer_enable();
+  IMU.gyroscope_enable();
+  IMU.temperature_enable();
+
+  delay(100);
+  
+  IMU.get_accel_data(&accel_data );
+  IMU.get_gyro_data(&gyro_data ); 
+  IMU.get_temperature(&temp );
+
+  /*
+   * ±16 g  : 2048  LSB/g 
+   * ±8 g   : 4096  LSB/g 
+   * ±4 g   : 8192  LSB/g 
+   * ±2 g   : 16384 LSB/g 
+   */
+  acc_x = (float)accel_data.x / 2048;
+  acc_y = (float)accel_data.y / 2048;
+  acc_z = (float)accel_data.z / 2048;
+    
+  Serial.print("Accel X: ");
+  Serial.print(acc_x);
+  Serial.print("[g]  Y: ");
+  Serial.print(acc_y);
+  Serial.print("[g]  Z: ");
+  Serial.print(acc_z);
+  Serial.println("[g]");
+
+  /*
+   * ±2000 º/s    : 16.4   LSB/(º/s)
+   * ±1000 º/s    : 32.8   LSB/(º/s)
+   * ±500  º/s    : 65.5   LSB/(º/s)
+   * ±250  º/s    : 131    LSB/(º/s)
+   * ±125  º/s    : 262    LSB/(º/s)
+   * ±62.5  º/s   : 524.3  LSB/(º/s)
+   * ±31.25  º/s  : 1048.6 LSB/(º/s) 
+   * ±15.625 º/s  : 2097.2 LSB/(º/s)
+   */
+  gyro_x = (float)gyro_data.x / 16.4;
+  gyro_y = (float)gyro_data.y / 16.4;
+  gyro_z = (float)gyro_data.z / 16.4;
+  
+  Serial.print("Gyro  X:");
+  Serial.print(gyro_x);
+  Serial.print("º/s  Y: ");
+  Serial.print(gyro_y);
+  Serial.print("º/s  Z: ");
+  Serial.print(gyro_z);
+  Serial.println("º/s");
+
+  Serial.print("Temper : ");
+  Serial.print(temp);
+  Serial.println("[ºC]");
+
+  IMU.accelerometer_disable();
+  IMU.gyroscope_disable();
+  IMU.temperature_disable();
+  IMU.idle();
+  delay(2000);
+			// Send the data package
+			if (sendLoRaFrame(acc_x, acc_y, acc_z))
+			{
+#ifndef MAX_SAVE
+				Serial.println("LoRaWan package sent successfully");
+#endif
+			}
+			else
+			{
+#ifndef MAX_SAVE
+				Serial.println("LoRaWan package send failed");
+				/// \todo maybe you need to retry here?
+#endif
+			}
+
+			break;
+		default:
+#ifndef MAX_SAVE
+			Serial.println("This should never happen ;-)");
+#endif
+			break;
+		}
+   digitalWrite(LED_BUILTIN, LOW);
+		// Go back to sleep
+		xSemaphoreTake(taskEvent, 10);
+	}
+}
